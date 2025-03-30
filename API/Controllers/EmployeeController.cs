f79cb8ecd16e4966cd49f31fb720edb32d0ddc7c
danielmutafov <danielmutafov@gmail.com>
2025-03-30 16:08:08 +0300

    final changes

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DTOs;
 using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -15,13 +16,14 @@ public class EmployeeController : ControllerBase
         _employeeService = employeeService;
     }
 
+    [Authorize(Roles = "HRAdmin")]
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateEmployeeRequestDto request)
     {
         var result = await _employeeService.CreateEmployee(request);
         return Ok(result);
     }
-
+    [Authorize(Roles = "HRAdmin,Manager")]
     [HttpGet]
     public async Task<IActionResult> GetEmployees([FromQuery] GetEmployeesRequestDto request)
     {
@@ -39,6 +41,7 @@ public class EmployeeController : ControllerBase
         return Ok(employee);
     }
 
+    [Authorize(Roles = "HRAdmin")]
     [HttpPut]
     public async Task<IActionResult> Update([FromBody] UpdateEmployeeRequestDto request)
     {
@@ -48,7 +51,7 @@ public class EmployeeController : ControllerBase
 
         return Ok(updated);
     }
-
+    [Authorize(Roles = "HRAdmin")]
     [HttpDelete("{id:guid}")]
     public async Task<IActionResult> Delete(Guid id)
     {
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-using Application.Interfaces;
+﻿using Application.Interfaces;
 using Application.Services;
 using Domain.Identity;
 using Infrastructure.Repositories;
@@ -6,37 +6,50 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Persistance;
+using System.Security.Claims;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80);
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "HCMApp API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 builder.Services.AddDbContext<HCMDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-            builder.Configuration["Jwt:SecretKey"]))
-    };
-});
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<HCMDbContext>()
     .AddDefaultTokenProviders();
@@ -45,20 +58,93 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            RoleClaimType = ClaimTypes.Role,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+        };
+    });
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<HCMDbContext>();
+    dbContext.Database.Migrate();
+}
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseStatusCodePages();
 app.UseHttpsRedirection();
-app.UseAuthentication();
+
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
-
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await SeedRolesAndAdmin(services);
+}
 app.Run();
+async Task SeedRolesAndAdmin(IServiceProvider services)
+{
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+    string[] roles = { "HRAdmin", "Manager", "Employee" };
+
+    foreach (var role in roles)
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+
+    var adminEmail = "admin@hcmapp.com";
+    var adminUser = await userManager.FindByEmailAsync(adminEmail);
+    var managerEmail = "manager@hcmapp.com";
+    var managerUser = await userManager.FindByEmailAsync(managerEmail);
+    if (adminUser == null)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            FullName = "System Admin",
+            Department = "HR"
+        };
+
+        var result = await userManager.CreateAsync(user, "Admin@123");
+
+        if (result.Succeeded)
+            await userManager.AddToRoleAsync(user, "HRAdmin");
+    }
+    if (managerUser == null)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = managerEmail,
+            Email = managerEmail,
+            FullName = "Manager",
+            Department = "Manager"
+        };
+
+        var result = await userManager.CreateAsync(user, "Manager@123");
+
+        if (result.Succeeded)
+            await userManager.AddToRoleAsync(user, "Manager");
+    }
+}
 public partial class Program { }
